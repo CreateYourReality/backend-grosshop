2541f3411c2fde5b7e5ef3b0a9fa15cc8b021583
Quaselz <paul.geppert94@freenet.de>
2023-08-22 14:22:03 +0200

    OrderItem setup

--- frontend/src/components/OrderItem/OrderItem.jsx
+++ frontend/src/components/OrderItem/OrderItem.jsx
@@ -1,10 +1,24 @@
 import "./OrderItem.css"
 const OrderItem = ({order}) => {
 
-    const {_id, invoice, transportStatus, payedStatus, date, user, products} = order
+    const {_id, invoice, transportStatus, payedStatus, date} = order
     return ( 
         <>
-            <h2>{user} Hier entsteht deine Ordershistoryitem</h2>
+            <article className="orderItem">
+                <div>
+                    <h3>{_id.slice(_id.length-6)}</h3>
+                    <p>{invoice}</p>
+                </div>
+                <div>
+                    <div>
+                        <div className={transportStatus}>{transportStatus}</div>
+                        <div className={payedStatus}>{payedStatus}</div>
+                    </div>
+                    <div>
+                        {date}
+                    </div>
+                </div>
+            </article>
         </>
      );
 }
