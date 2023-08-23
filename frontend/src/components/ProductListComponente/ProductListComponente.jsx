49eb9e37bb1eb4449ca2902ce8be1a0301d21451
CreateYourReality <timo.moewes@icloud.com>
2023-08-23 15:48:58 +0200

    add deals

--- frontend/src/components/ProductListComponente/ProductListComponente.jsx
+++ frontend/src/components/ProductListComponente/ProductListComponente.jsx
@@ -4,6 +4,8 @@ import { categoryContext, dataContext,priceContext } from "../../context/Context
 import ProductCard from "../ProductCard/ProductCard";
 
 import deals from "../../assets/img/deals.png"
+import deals2 from "../../assets/img/deals2.png"
+import { UserContext } from "../../context/UserContext";
 
 
 const ProductListComponente = () => {
@@ -11,6 +13,7 @@ const ProductListComponente = () => {
     const {categoryFilter} = useContext(categoryContext)
     const {priceFilter} = useContext(priceContext)
     const [filteredData,setFilteredData] = useState(data)
+    const {user} = useContext(UserContext)
 
     useEffect(() => {
         let newFilteredData = data;
@@ -42,7 +45,10 @@ const ProductListComponente = () => {
                     .map((product, index) => (
                         <ProductCard key={index} product={product} />
                     ))}
-                <img className="deals-img" src={deals} alt="deals img" />
+                {location.pathname == "/home" ?
+                    <img className="deals-img" src={deals} alt="deals img" />
+                    :<img className="deals-img" src={deals2} alt="deals2 img" />
+                }
             </>
         ) : (
             <p>No products with deals tag found.</p>
@@ -51,8 +57,30 @@ const ProductListComponente = () => {
         <p>Fetching data...</p>
     )}
 </section>
+
+
+{location.pathname == "/home" ?<h2 className="groceryDeal">Today Grocery Member Deals</h2>:""}
+            <section className="product-list deal-list">
+    {data && location.pathname == "/home"? (
+        data.length > 0 ? (
+            <>
+                {data
+                    .filter(product => product.price > 0) //TODO isMemberDeal
+                    .slice(0, 3)
+                    .map((product, index) => (
+                        <ProductCard key={index} product={product} />
+                    ))}
+            </>
+        ) : (
+            <p>No products with member deals tag found.</p>
+        )
+    ) : (
+        location.pathname == "/home"?<p>Fetching data...</p>:""
+    )}
+</section>
+
             <section className="product-list">
-              {data? 
+              {data && location.pathname == "/productlist"? 
                 filteredData.map((product,index) => {
                     return(
                         <ProductCard key={index} product={product}/>
