7ea6a1a9d5b6d63d9cbac0ab77c66b26f79deae1
Quaselz <paul.geppert94@freenet.de>
2023-08-25 16:57:31 +0200

    select checkout fix

--- frontend/src/components/TotalCost/TotalCost.jsx
+++ frontend/src/components/TotalCost/TotalCost.jsx
@@ -19,9 +19,9 @@ const TotalCost = () => {
       return selectedCartItems.reduce((total, cartItem) => {
           const cartProduct = data.find(prod => prod._id == cartItem);
           console.log(cartProduct);
+          //console.log(userShoppingCart);
           if (cartProduct) {
-              const cartAmount = userShoppingCart.find(cartItem => cartItem._id == cartProduct._id).amount;
-              //console.log();
+              const cartAmount = userShoppingCart.find(cartItem => cartItem.id == cartProduct._id).amount;
               const itemTotal = cartProduct.price * cartAmount
               return total + itemTotal;
           } else {
